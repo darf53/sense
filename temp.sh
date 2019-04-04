84fdadd30d1f6f784fbf6b144503fd02e189ec92
jef53 <jef53@telenet.be>
2019-04-04 22:18:36 +0200

    Added comment on type sensor to use

--- temp.sh
+++ temp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# script for reading a temp sensor on the RaspberryPI
+# connect a sensor type DS18b20 
+
 temp=`python /home/pi/sense/temp.py`
 
 curl -i "https://influx.effwd.be/write?db=bommel" --data-binary "temp,location=living value=$temp"
