6cd91155ef2a0f439d88c2dba7882af58b1e8e1d
jef53 <jef53@telenet.be>
2019-04-04 21:43:05 +0200

    test

--- temp.sh
+++ temp.sh
@@ -3,4 +3,4 @@
 temp=`python /home/pi/sense/temp.py`
 
 curl -i "https://influx.effwd.be/write?db=bommel" --data-binary "temp,location=living value=$temp"
-#echo "uploading temp data to influxdb - exit code:" $?
+# echo "uploading temp data to influxdb - exit code:" $?
